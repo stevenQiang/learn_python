216440ea698023a19d5fdef69b4d77ddddd83904
gaoqiang <qiang.gao@coalink.com>
2016-09-08 17:05:42 +0800

    add two numbers

--- feetcode/Add_Two_Numbers/add_two_numbers.py
+++ feetcode/Add_Two_Numbers/add_two_numbers.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# 链表操作
+# 当情况为0, 0的时候，当情况为5, 5,出错
+
 
 class ListNode(object):
     def __init__(self, x):
@@ -7,8 +10,29 @@ class ListNode(object):
         self.next = None  # 指针域
 
 
-def add_two_number(l1, l2):
-    head = ListNode(0)
+def add(l1, l2):
+    result = ListNode(0)
+    current = result
+    total = 0
+    output = []
+    while l1 or l2 or total != 0:
+        val = total
+        if l1:
+            val += l1.val
+            l1 = l1.next
+        if l2:
+            val += l2.val
+            l2 = l2.next
+        total, val = val / 10, val % 10
+        current.next = ListNode(val)
+        current = current.next
+        print total
+        output.append(val)
+    print output
+    return output
+
 
 if __name__ == '__main__':
-    add_two_number([2, 4, 3], [5, 6, 4])
+    a = ListNode(5)
+    b = ListNode(5)
+    add(a, b)
