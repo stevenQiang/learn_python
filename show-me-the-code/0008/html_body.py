c3a9c2f6b83f4762661d8864bdd9b7af5439386b
gaoqiang <qiang.gao@coalink.com>
2016-09-09 15:07:55 +0800

    8 & 9

---
+++ show-me-the-code/0008/html_body.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# 第 0008 题：一个HTML文件，找出里面的正文。
+import requests
+import urllib2
+# Beautiful Soup 是一个可以从HTML或XML文件中提取数据的Python库
+from bs4 import BeautifulSoup
+
+
+def html_body(url):
+    r = urllib2.urlopen(url)
+    html = BeautifulSoup(r)
+    print html.body.text
+
+if __name__ == "__main__":
+    url = 'http://docs.python-requests.org/zh_CN/latest/user/advanced.html'
+    html_body(url)
