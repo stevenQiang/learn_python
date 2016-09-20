e16fa424d03794e42eea2dc03988b9562c61fda6
gaoqiang <qiang.gao@coalink.com>
2016-09-20 13:30:02 +0800

    main

--- main-point/代替switch语句/file.py
+++ main-point/代替switch语句/file.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+
+def func(a):
+    return {
+        'a': 1,
+        'b': 2,
+        'c': 3
+    }.get(a, 4)   # get可以设置一下默认值，如果没有找到的话，默认值就是4
+
+if __name__ == "__main__":
+    print func('4')
